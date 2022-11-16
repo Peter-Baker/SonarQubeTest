e0e34b988299ce4046d68ef6d4ae88d097a27f73
Peter-Baker <peterbaker0213@gmail.com>
2022-11-16 11:38:32 -0500

    Added test code

---
+++ test.py
@@ -0,0 +1,9 @@
+
+# Test code to show of SonarQube
+# By: Peter Baker
+
+username = input("Please enter your username")
+password = input("Please enter your password")
+
+print(f"Your username is " + username)
+print(f"Your password is " + password)
