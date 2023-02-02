3bd44e761df559da8f3162ba1d17e9a0ccad1382
peter-baker-tjx <pet01020@tjx.com>
2023-02-02 09:12:59 -0500

    Added account file, removed test files

--- test.py
+++
@@ -1,9 +0,0 @@
-
-# Test code to show of SonarQube
-# By: Peter Baker
-
-username = input("Please enter your username")
-password = input("Please enter your password")
-
-print(f"Your username is " + username)
-print(f"Your password is " + password)
